a6ada309e8d6835e4ab99d5ec0898729d19b91a7
alam.hutomo <alam.hutomo@spesolution.com>
2024-06-27 16:49:44 +0700

    Improve

--- assets/js/script.js
+++ assets/js/script.js
@@ -164,14 +164,13 @@ function timeAgo(date) {
 
 function openModal(){
   // Get the modal
-    var modal = document.getElementById("myModal");
+  var modal = document.getElementById("myModal");
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("closemodal")[0];
     // When the user clicks the button, open the modal 
     btn.onclick = function() {
       modal.style.display = "block";
