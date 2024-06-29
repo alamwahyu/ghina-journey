6184e83a3718b5dbe74c6a234e243a06a8bed131
alam.hutomo <alam.hutomo@spesolution.com>
2024-06-29 18:35:15 +0700

    Improve

--- assets/js/script.js
+++ assets/js/script.js
@@ -162,29 +162,39 @@ function timeAgo(date) {
             }
         }
 
-function openModal(){
-  // Get the modal
+// Function to open the modal
+function openModal(event, element) {
+  event.preventDefault(); // Prevent the default action (navigation)
+  var modal = document.getElementById("myModal");
+  var modalBody = document.getElementById("modalBody");
+
+  // Get the content from data-content attribute of the clicked element
+  var content = element.getAttribute("data-content");
+
+  // Set the content of the modal body
+  modalBody.innerHTML = `<p class="card-text">${content}</p>`;
+
+  // Display the modal
+  modal.style.display = "block";
+}
+
+// Function to close the modal
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+// Function to initialize event listeners for closing modal by clicking outside of it
+function initializeModal() {
   var modal = document.getElementById("myModal");
 
-  // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("closemodal")[0];
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
       if (event.target == modal) {
-        modal.style.display = "none";
+          closeModal();
       }
-    }
+  }
 }
+
+// Initialize modal functionality when the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", initializeModal);
